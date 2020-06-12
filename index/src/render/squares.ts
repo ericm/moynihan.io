1fda4abb7126570847d5b3d59a1fbe063a99844c
Eric Moynihan <git@moynihan.io>
2020-06-12 23:33:18 +0100

    Added state to squares

--- index/src/render/squares.ts
+++ index/src/render/squares.ts
@@ -2,19 +2,41 @@ import Two from 'two.js';
 
 type Rect = Two.Rectangle;
 
+const SIZE = 140;
+const RATIO = 2.64;
+
+interface State {
+  rotation: number;
+  size?: number;
+  coords?: { x: number; y: number };
+}
+
+interface Theme {
+  primary: Two.Color;
+  secondary: Two.Color;
+  state: [State, State, State, State, State];
+}
+
+const themes: { [name: string]: Theme } = {
+  primary: {
+    primary: '#EA2099',
+    secondary: '#92145F',
+    state: [
+      { rotation: 30 * (Math.PI / 180) },
+      { rotation: 45 * (Math.PI / 180) },
+      { rotation: 60 * (Math.PI / 180) },
+      { rotation: 75 * (Math.PI / 180) },
+      { rotation: 135 * (Math.PI / 180), size: SIZE / RATIO },
+    ],
+  },
+};
+
 export default class Squares {
-  public static SIZE = 140;
-  public static RATIO = 2.64;
   private $root: Two;
   private $rects: [Rect, Rect, Rect, Rect, Rect];
   constructor(root: HTMLDivElement) {
-    this.$root = new Two({ fullscreen: false, autostart: true }).appendTo(root);
-    const config: [number, number, number, number] = [
-      this.$root.width / 2,
-      this.$root.height / 2,
-      Squares.SIZE,
-      Squares.SIZE,
-    ];
+    this.$root = new Two({ fullscreen: true, autostart: true }).appendTo(root);
+    const config = this.getConfig();
     this.$rects = [
       this.$root.makeRectangle(...config),
       this.$root.makeRectangle(...config),
@@ -23,9 +45,29 @@ export default class Squares {
       this.$root.makeRectangle(
         config[0],
         config[1],
-        Squares.SIZE / Squares.RATIO,
-        Squares.SIZE / Squares.RATIO
+        SIZE / RATIO,
+        SIZE / RATIO
       ),
     ];
+    this.setStyles('primary');
   }
+  private getConfig = (): [number, number, number, number] => [
+    this.$root.width / 2,
+    this.$root.height / 2,
+    SIZE,
+    SIZE,
+  ];
+  private setStyles = (name: string) => {
+    const theme = themes[name];
+    for (let i in this.$rects) {
+      if (+i % 2 === 0) {
+        this.$rects[i].fill = theme.secondary;
+      } else {
+        this.$rects[i].fill = theme.primary;
+      }
+      this.$rects[i].rotation = theme.state[i].rotation;
+      this.$rects[i].noStroke();
+    }
+    this.$root.render();
+  };
 }
