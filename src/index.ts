e481ca2db3c2d92e9935ffc282d19cf308266579
Eric Moynihan <git@moynihan.io>
2020-12-29 19:54:01 +0000

    Added vrouter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const Root = document.querySelector('main') as HTMLDivElement;
+function init() {
+  const root = document.querySelector('main') as HTMLDivElement;
+
+  const url = document.URL.split('/');
+  const slug = url.reduce((p, c) => (c !== '' ? c : p));
+
+  route(slug);
+}
+
+document.addEventListener('load', init);
+window.addEventListener('locationchange', init);
---
+++ src/router.ts
@@ -0,0 +1,10 @@
+function route(slug: string) {
+  switch (slug) {
+    case 'projects':
+      break;
+    case 'cv':
+      break;
+    case 'photography':
+      break;
+  }
+}
