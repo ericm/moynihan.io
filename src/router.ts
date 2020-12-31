92da8fa2835eca34301918eff85b4909b894b11d
Eric Moynihan <git@moynihan.io>
2020-12-31 14:15:49 +0000

    Mobile optimized nav

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,17 @@
 export function route(slug: string) {
+  const links = document.querySelectorAll('.bar a');
+  links.forEach((link) => (link.id = ''));
   switch (slug) {
     case 'projects':
       window.history.replaceState({}, 'Projects | Eric Moynihan', '/projects');
+      links[0].id = 'selected';
       break;
     case 'cv':
       window.history.replaceState({}, 'CV | Eric Moynihan', '/cv');
+      links[1].id = 'selected';
       break;
     case 'photography':
+      links[2].id = 'selected';
       window.history.replaceState(
         {},
         'Photography | Eric Moynihan',
