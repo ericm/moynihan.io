3a981208cc7d214d89e4554a3f0d6cf692fc53fb
Eric Moynihan <git@moynihan.io>
2021-01-15 18:49:22 +0000

    Added gallery

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,10 @@ import marked from 'marked';
 
 const projects =
     'https://raw.githubusercontent.com/ericm/projects/master/README.md',
-  cv = 'https://raw.githubusercontent.com/ericm/cv/master/README.md';
+  cv = 'https://raw.githubusercontent.com/ericm/cv/master/README.md',
+  gallery = 'https://api.github.com/repos/ericm/photography/contents/gallery',
+  thumbnails =
+    'https://api.github.com/repos/ericm/photography/contents/thumbnails';
 
 const pages: string[] = ['Projects', 'CV', 'Photography'];
 
@@ -26,7 +29,22 @@ export async function route(slug: string) {
       }, 1);
       break;
     case 'photography':
-      render(async () => {}, 2);
+      render(async () => {
+        let buffer = `<ul style="display: flex; flex-wrap: wrap;">`;
+        const galleryResp = (await (await fetch(gallery)).json()) as {
+          name: string;
+          download_url: string;
+          type: string;
+          path: string;
+        }[];
+        for (let image of galleryResp) {
+          let thumbnail = image.download_url.replace('gallery/', 'thumbnails/');
+          buffer += `<li style="margin: 0; padding: 0; display: flex; height: 30vh; flex-grow: 1;"><img style="border-radius: 0; max-height: 100%; min-width: 100%; object-fit: cover; vertical-align: top; margin: 0;" src="${thumbnail}"/></li>`;
+        }
+        buffer += `</ul>`;
+        console.log(galleryResp);
+        root.innerHTML = buffer;
+      }, 2);
       break;
     default:
       document.querySelector('.bar')!!.id = '';
