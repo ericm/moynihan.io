5691c03f25dfa592780edd88dc241ad77e0b0f09
Eric Moynihan <git@moynihan.io>
2021-02-01 11:10:06 +0000

    Added CV fix

--- src/router.ts
+++ src/router.ts
@@ -27,8 +27,9 @@ export async function route(slug: string, full: string) {
       break;
     case 'cv':
       render(async () => {
-        document.body.innerHTML += `<a download="Eric Moynihan CV" id="download" href="${downloadCV}">Download CV 🗎</a>`;
-        root.innerHTML = marked(await (await fetch(cv)).text());
+        root.innerHTML =
+          marked(await (await fetch(cv)).text()) +
+          `<a download="Eric Moynihan CV" id="download" href="${downloadCV}">Download CV 🗎</a>`;
       }, 1);
       break;
     case 'photography':
